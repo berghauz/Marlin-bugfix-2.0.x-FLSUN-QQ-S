c34f10b95bbf17e5dedad1a73de8b1689fff971c
berghauz <berghauz@gmail.com>
2019-05-25 03:07:10 +0300

    version update

--- Marlin/src/inc/Version.h
+++ Marlin/src/inc/Version.h
@@ -38,7 +38,7 @@
   /**
    * Marlin release version identifier
    */
-  #define SHORT_BUILD_VERSION "bugfix-2.0.x"
+  #define SHORT_BUILD_VERSION "bugfix-2.0.x-c1"
 
   /**
    * Verbose version identifier which should contain a reference to the location
